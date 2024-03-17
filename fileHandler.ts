2c2837b215e7e40c0f5e02076ff62fe119c6353b
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-17 12:39:12 +0600

    created file manipulation file

---
+++ fileHandler.ts
@@ -0,0 +1,34 @@
+import {promises as fs} from 'fs';
+import {Message} from './types';
+import Messages from './routers/messages';
+
+
+const fileHandler = {
+  path: './messages/',
+  async saveMessage ({message, datetime}: Message) {
+    const fileName = this.path + datetime + '.txt';
+    const data = {message};
+    await fs.writeFile(fileName, JSON.stringify(data, null, 2));
+  },
+  async getMessages () {
+    const files = await fs.readdir(this.path);
+    let data: Message[] = [];
+
+    for (const file of files.slice(-5)) {
+      const message = await this.readMessage(file);
+      data.push(message);
+    }
+    return data;
+  },
+  async readMessage(fileName: string) {
+    try {
+      const fileContents = await fs.readFile(this.path + fileName);
+      return JSON.parse(fileContents.toString());
+    }
+    catch (e) {
+      console.error('Error read file');
+    }
+  },
+};
+
+export default fileHandler;
